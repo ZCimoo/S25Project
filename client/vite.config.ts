2c8e16da4017d172754c52f7de170f3cbf19650f
Zachary <zachary.cimorelli09@gmail.com>
2025-03-18 10:05:37 -0400

    Updated vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,10 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: '/<REPO_NAME>/', // Replace <REPO_NAME> with your actual repo name
-  build: {
-    outDir: 'dist', // Ensures built files stay inside client/dist
-  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
