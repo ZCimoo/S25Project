dc281e7cbd9316628ba51bb305ee3a7928ebbd80
Zachary <zachary.cimorelli09@gmail.com>
2025-04-29 21:36:30 -0400

    added .env and updated to serve static files

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
+require("dotenv").config();
 const activitiesController = require("./controllers/activities");
 const usersController = require("./controllers/users");
-const PORT = 8000;
+const PORT = process.env.PORT ?? 8000;
 
 const app = express();
 //Middleware
 app.use(express.json());
+//Controllers
 app
   .get("/", (req, res) => {
     res.send("Hello World");
   })
   .use("/api/v1/activities", activitiesController)
-  .use("/api/v1/users", usersController);
-
+  .use("/api/v1/users", usersController)
+  .use("/", express.static("dist"));
+//Error handling
 app.use((err, req, res, next) => {
   console.error(err);
   const status = err.status || 500;
@@ -24,7 +27,8 @@ app.use((err, req, res, next) => {
   res.status(status).send(error);
 });
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Welcome to the best class at New Paltz - ${process.env.BEST_CLASS}
+    Server is running on port http://localhost:${PORT}`);
 });
 
 console.log("Hello World");
