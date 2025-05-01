7a9a0670de14339ca34028c9cc6c7f7c3d8a1c68
Zachary <zachary.cimorelli09@gmail.com>
2025-04-30 23:54:14 -0400

    4th deploy attempt

--- server/index.js
+++ server/index.js
@@ -9,12 +9,16 @@ const app = express();
 //Middleware
 // CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigin = "https://dynamics25project.onrender.com";
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  res.header("Access-Control-Allow-Credentials", "true");
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
